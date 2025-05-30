8e872ee72d92f1fe9cf52ece3e6d849e3a3f39e0
Yusuff Olatunji Sikiru <60070718+Seek-Techs@users.noreply.github.com>
2025-05-30 13:13:29 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -398,7 +398,7 @@ def projects_page_content():
             st.session_state.selected_project_id = selected_project_id_from_df
             st.session_state.selected_project_name = projects_df[projects_df['id'] == selected_project_id_from_df]['project_name'].iloc[0]
             st.success(f"Selected project: {st.session_state.selected_project_name}. Go to 'Tasks' tab to manage it.")
-            st.markdown(f"**Selected project**: {st.session_state.selected_project_name}. Go to 'Tasks' tab to manage it.")
+            # st.markdown(f"**Selected project**: {st.session_state.selected_project_name}. Go to 'Tasks' tab to manage it.")
         else:
             st.session_state.selected_project_id = None
             st.session_state.selected_project_name = None
